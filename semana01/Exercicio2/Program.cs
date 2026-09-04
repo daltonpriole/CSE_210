4acd808aa2de92716fcf0f1d1c9234d0a4264db1
daltonpriole <tonpriole@gmail.com>
2026-09-04 18:56:23 -0300

    Exer2_Finalizado_18550409

--- semana01/Exercicio2/Program.cs
+++ semana01/Exercicio2/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
+    // Esse programa solicita a nota do aluno de 0 a 100 e devolve a nota conceitual de A até F
+    // Realizado o adicional de sinais + se o segundo digito for maior ou igual a 7 e - se o segundo digito for menor ou igual a 3
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio2.");
+        int nota;
+        string sinal = "";
+        string conceito = "";
+        Console.Write("Digite a nota do aluno (0-100): ");
+        nota = int.Parse(Console.ReadLine());
+
+        if (nota >= 90)
+            conceito = "A";
+        else if (nota >= 80)
+            conceito = "B";
+        else if (nota >= 70)
+            conceito = "C";
+        else if (nota >= 60)
+            conceito = "D";
+        else
+            conceito = "F";
+
+        if (nota % 10 >= 7)
+            sinal = "+";
+        else if (nota % 10 <= 3)
+            sinal = "-";
+
+        Console.WriteLine($"Sua nota é {conceito}{sinal}");
     }
 }
